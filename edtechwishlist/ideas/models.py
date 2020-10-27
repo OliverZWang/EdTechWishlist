3355c8fb5269329d349fff978bf5125dfdcb7169
OliverZWang <zw1400@nyu.edu>
2020-10-27 10:46:15 -0400

    reorder-tweets-reset-form-require-content

--- edtechwishlist/ideas/models.py
+++ edtechwishlist/ideas/models.py
@@ -5,6 +5,11 @@ class Idea(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='image/', blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id']
+
+
+
     def serialize(self):
         return{
             "id": self.id,
