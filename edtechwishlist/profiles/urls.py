98c18535e093c3ae704c68db0f48160979936072
OliverZWang <zw1400@nyu.edu>
2020-11-19 09:16:32 -0500

    profile update page

--- edtechwishlist/profiles/urls.py
+++ edtechwishlist/profiles/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
+
 
 
 urlpatterns = [
 
     path('<str:username>', profile_detail_view),
+    path('edit/', profile_update_view),
 
 ]
